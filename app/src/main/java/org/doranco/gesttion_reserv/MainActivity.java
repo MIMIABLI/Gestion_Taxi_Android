7773d5726aa0bbf233b4d81bc3c24806a74f3db9
AmaxFR <amalaitagames@protonmail.com>
2023-02-23 00:12:23 +0100

    Correction error pour run l'application à cause de null pointer sur CreationCompteClient

--- app/src/main/java/org/doranco/gesttion_reserv/MainActivity.java
+++ app/src/main/java/org/doranco/gesttion_reserv/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity   {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page_connexion);
-        this.button = findViewById(R.id.btn);
+        this.button = (Button) findViewById(R.id.btn);
 
         button.setOnClickListener(new View.OnClickListener(){
             @Override
