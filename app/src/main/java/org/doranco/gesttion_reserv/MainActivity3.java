9be10cebbf9cb494e8720d8b6b710a1ac7ff5379
AmaxFR <amalaitagames@protonmail.com>
2023-02-23 19:42:22 +0100

    Correction compte administrateur et page paiement .xml; Réimplémentation de la liste chauffeur en recyclerView dans MainActivity3

--- app/src/main/java/org/doranco/gesttion_reserv/MainActivity3.java
+++ app/src/main/java/org/doranco/gesttion_reserv/MainActivity3.java
@@ -3,12 +3,65 @@ package org.doranco.gesttion_reserv;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.doranco.models.Chauffeur;
+import org.doranco.models.MyAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity3 extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    List<Chauffeur> chauffeurList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.page_reservation);
+        setContentView(R.layout.page_trajet);
+
+        Chauffeur chauffeur1 = new Chauffeur();
+        chauffeur1.setNom("Riri");
+        chauffeur1.setPrenom("Koko");
+        chauffeur1.setTypeDeVehicules("306");
+        chauffeur1.setNote(4.5);
+        chauffeur1.setPrix(20.5);
+        chauffeur1.setPhotos(R.drawable.avatar_homme);
+
+        Chauffeur chauffeur2 = new Chauffeur();
+        chauffeur2.setNom("Rara");
+        chauffeur2.setPrenom("Koukou");
+        chauffeur2.setTypeDeVehicules("Mazda 3");
+        chauffeur2.setNote(4.5);
+        chauffeur2.setPrix(20.5);
+        chauffeur2.setPhotos(R.drawable.fille);
+
+        Chauffeur chauffeur3 = new Chauffeur();
+        chauffeur3.setNom("Ruru");
+        chauffeur3.setPrenom("Kake");
+        chauffeur3.setTypeDeVehicules("Clio 3");
+        chauffeur3.setNote(4.5);
+        chauffeur3.setPrix(20.5);
+        chauffeur3.setPhotos(R.drawable.garcon);
+
+        Chauffeur chauffeur4 = new Chauffeur();
+        chauffeur4.setNom("Rere");
+        chauffeur4.setPrenom("Kiko");
+        chauffeur4.setTypeDeVehicules("Audi A3");
+        chauffeur4.setNote(4.5);
+        chauffeur4.setPrix(20.5);
+        chauffeur4.setPhotos(R.drawable.avatar_homme);
+
+
+        chauffeurList.add(chauffeur1);
+        chauffeurList.add(chauffeur2);
+        chauffeurList.add(chauffeur3);
+        chauffeurList.add(chauffeur4);
+
+        recyclerView = findViewById(R.id.listChauffeur);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new MyAdapter(getApplicationContext(), chauffeurList));
     }
 }
